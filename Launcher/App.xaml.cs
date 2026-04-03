f64362a1d2b61b573d9d69eefbfee799f4229454
x64-dev <202863051+x64-dev@users.noreply.github.com>
2026-04-02 20:17:42 -0400

    - Add sentry for error tracking

--- Launcher/App.xaml.cs
+++ Launcher/App.xaml.cs
@@ -1,4 +1,6 @@
+using Sentry;
 using System.Windows;
+using System.Windows.Threading;
 
 namespace LauncherWinUI
 {
@@ -6,9 +8,28 @@ namespace LauncherWinUI
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+            SentrySdk.Init(options =>
+            {
+                options.Dsn = "https://c5fbadac932626fcef118351949df55f@o4509316925554688.ingest.us.sentry.io/4511153204756480";
+                options.AutoSessionTracking = true;
+                options.Environment = "production";
+                options.Release = "generalsonline-launcher@040226";
+#if DEBUG
+                options.Debug = true;
+#endif
+            });
+
+            DispatcherUnhandledException += OnDispatcherUnhandledException;
+
             base.OnStartup(e);
             var window = new MainWindow();
             window.Show();
+        }
+
+        private static void OnDispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            SentrySdk.CaptureException(e.Exception);
+            SentrySdk.Flush(System.TimeSpan.FromSeconds(2));
         }
     }
 }
